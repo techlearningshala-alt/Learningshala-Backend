50b77a1dcefde1f6571430ee7589dc445613d532
techlearningshala-alt <techlearningshala@gmail.com>
2026-03-23 16:36:47 +0530

    author slug added

--- src/repositories/author.repository.ts
+++ src/repositories/author.repository.ts
@@ -19,16 +19,17 @@ export class AuthorRepository {
     }
 
   async findBySlug(slug: string): Promise<Author | null> {
-    const [rows]: any = await pool.query("SELECT authors.id, authors.author_name, authors.image, authors.author_details, authors.label, authors.author_slug, b.h1_tag as blog_title, b.short_description as blog_short_description, b.thumbnail as blog_thumbnail , b.slug as blog_slug,b.verified as blog_verified, b.updated_at as blog_updated_at, b.meta_title as blog_meta_title, b.meta_description as blog_meta_description FROM authors left join blogs b on authors.id = b.author_id WHERE author_slug = ?", [slug]);
-    const author_blogs: { title: string | null, short_description: string | null, thumbnail: string | null, slug: string | null, verified: boolean | null, update_date: Date | null, meta_title: string | null, meta_description: string | null }[] = rows.map((row: any) => ({
+    const [rows]: any = await pool.query("SELECT authors.id, authors.author_name, authors.image, authors.author_details, authors.label, authors.author_slug, b.h1_tag as blog_title, b.short_description as blog_short_description, b.thumbnail as blog_thumbnail , b.slug as blog_slug,b.verified as blog_verified, b.updated_at as blog_updated_at, b.meta_title as blog_meta_title, b.meta_description as blog_meta_description, bc.title as category_title FROM authors left join blogs b on authors.id = b.author_id left join blog_categories bc on b.category_id = bc.id WHERE author_slug = ?", [slug]);
+    const author_blogs: { title: string | null, short_description: string | null, thumbnail: string | null, slug: string | null, verified: boolean | null, updated_at: Date | null, meta_title: string | null, meta_description: string | null, category_title: string | null}[] = rows.map((row: any) => ({
       h1_tag: row.blog_title,
       short_description: row.blog_short_description,
       thumbnail: row.blog_thumbnail,
       slug: row.blog_slug,
       verified: row.blog_verified,
-      update_at: row.blog_updated_at,
+      updated_at: row.blog_updated_at,
       meta_title: row.blog_meta_title,
       meta_description: row.blog_meta_description,
+      category_title: row.category_title,
     }));
     return rows.length ? { id: rows[0].id, author_name: rows[0].author_name, image: rows[0].image, author_details: rows[0].author_details, label: rows[0].label, author_slug: rows[0].author_slug, created_at: rows[0].created_at, updated_at: rows[0].updated_at, author_blogs } : null;
   }
